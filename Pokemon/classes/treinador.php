66a8ce2e38d294f8b388db0a1cba666c3ddd1ba2
MateusCarvalho2007 <mateusreichert2007@gmail.com>
2025-10-02 17:29:47 -0300

    Add files via upload

---
+++ Pokemon/classes/treinador.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once __DIR__."\..\bd\MySQL.php";
+
+class Treinador {
+    private int $idTreinador;
+    
+    public function __construct(private string $nome,private string $email,private string $senha){
+    }
+
+    public function setIdTreinador(int $idTreinador): void {
+        $this->idTreinador = $idTreinador;
+    }
+
+
+    public function getIdTreinador(): ?int {
+        return $this->idTreinador;
+    }
+
+
+    public function setSenha(string $senha): void {
+        $this->senha = $senha;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+
+    public function getSenha(): string {
+        return $this->senha;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function save(): bool{
+        $conexao = new MySQL();
+        $this->senha = password_hash($this->senha, PASSWORD_BCRYPT);
+        if (isset($this->idTreinador)) {
+            $sql = "UPDATE treinador SET email = '{$this->email}', senha = '{$senhaHash}' WHERE idTreinador = {$this->idTreinador}";
+        } else {
+            $sql = "INSERT INTO treinador (email, senha) VALUES ('{$this->email}', '{$senhaHash}')";
+        }
+        return $conexao->executa($sql);
+    }
+
+    public function authenticate():bool{
+        $conexao = new MySQL();
+        $sql = "SELECT idTreinador, email, senha FROM treinador WHERE email = '{$this->email}'";
+        $resultados = $conexao->consulta($sql);
+        if(count($resultados)>0){
+            if(password_verify($this->senha, $resultados[0]['senha'])){
+                session_start();
+                $_SESSION['idTreinador'] = $resultados[0]['idTreinador'];
+                $_SESSION['email'] = $resultados[0]['email'];
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
---
+++ Pokemon/teste.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__."/classes/Usuario.php";
+
+$t1 = new Usuario("Ash","50");
+$t1->save();
+
+
+?>
