2da1d1fa688949b4fddddd25255b5edf2f0ac27b
rafaelkfw <rafaelkleinfuhr@gmail.com>
2025-10-09 16:54:40 -0300

    p

--- Pokemon/index.php
+++ Pokemon/index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela inicial</title>
+    <title>inicio box de troca pokemon</title>
 </head>
 <body>
-    <a href='loginForm.php'>Fazer login</a>  
+    <h1><strong>Seja bem vindo</strong></h1>
+    <br>
+    <p><b>azer login</b></p>
+        <a href='loginForm.php'>Fazer login aqui</a> 
+    <br>
+    <p><b>Criar conta</b></p>
+         <a href='formCadTreinador.php'>cadastre treinador aqui</a> 
 </body>
 </html>
