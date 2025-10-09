a76a0a0bfcdc6ce00136581c8e857bf395cb4591
MateusCarvalho2007 <mateusreichert2007@gmail.com>
2025-10-09 16:12:37 -0300

    efnsdckjweghudf

--- Pokemon/trocaspokemon.sql
+++ Pokemon/trocaspokemon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02/10/2025 às 22:36
+-- Tempo de geração: 09/10/2025 às 20:35
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `pokemon` (
   `idPokemon` int(11) NOT NULL,
+  `idTreinador` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `descricao` varchar(200) NOT NULL,
   `tipo` text NOT NULL
@@ -47,6 +48,20 @@ CREATE TABLE `treinador` (
   `nome` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `troca`
+--
+
+CREATE TABLE `troca` (
+  `idTreinador1` int(11) NOT NULL,
+  `idTreinador2` int(11) NOT NULL,
+  `idPokemon1` int(11) NOT NULL,
+  `idTroca` int(11) NOT NULL,
+  `idPokemon2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tabelas despejadas
 --
@@ -55,13 +70,24 @@ CREATE TABLE `treinador` (
 -- Índices de tabela `pokemon`
 --
 ALTER TABLE `pokemon`
-  ADD PRIMARY KEY (`idPokemon`);
+  ADD PRIMARY KEY (`idPokemon`),
+  ADD KEY `pokemon_ibfk_1` (`idTreinador`);
 
 --
 -- Índices de tabela `treinador`
 --
 ALTER TABLE `treinador`
   ADD PRIMARY KEY (`idTreinador`);
+
+--
+-- Índices de tabela `troca`
+--
+ALTER TABLE `troca`
+  ADD PRIMARY KEY (`idTroca`),
+  ADD KEY `idTreinador1` (`idTreinador1`),
+  ADD KEY `idTreinador2` (`idTreinador2`),
+  ADD KEY `idPokemon` (`idPokemon1`),
+  ADD KEY `idPokemon2` (`idPokemon2`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -78,6 +104,31 @@ ALTER TABLE `pokemon`
 --
 ALTER TABLE `treinador`
   MODIFY `idTreinador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `troca`
+--
+ALTER TABLE `troca`
+  MODIFY `idTroca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `pokemon`
+--
+ALTER TABLE `pokemon`
+  ADD CONSTRAINT `pokemon_ibfk_1` FOREIGN KEY (`idTreinador`) REFERENCES `treinador` (`idTreinador`);
+
+--
+-- Restrições para tabelas `troca`
+--
+ALTER TABLE `troca`
+  ADD CONSTRAINT `troca_ibfk_1` FOREIGN KEY (`idTreinador1`) REFERENCES `treinador` (`idTreinador`),
+  ADD CONSTRAINT `troca_ibfk_2` FOREIGN KEY (`idTreinador2`) REFERENCES `treinador` (`idTreinador`),
+  ADD CONSTRAINT `troca_ibfk_3` FOREIGN KEY (`idPokemon1`) REFERENCES `pokemon` (`idPokemon`),
+  ADD CONSTRAINT `troca_ibfk_4` FOREIGN KEY (`idPokemon2`) REFERENCES `pokemon` (`idPokemon`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
